befecd730249f8271b3c5b84218b0a3e1f56eb2e
Kurt Dacudag <dcdgblessie18@gmail.com>
2018-04-23 14:24:11 +0800

    Make method protected

--- src/Globe.php
+++ src/Globe.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 class Globe extends Facade
 {
-	public static function getFacadeAccessor()
+	protected static function getFacadeAccessor()
 	{
 		return 'globe';
 	}
