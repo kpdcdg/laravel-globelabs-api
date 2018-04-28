fc8598e996e84a4cb0cc21c121e2bd133d712e49
Kurt Dacudag <dcdgblessie18@gmail.com>
2018-04-28 13:22:58 +0800

    Fix variable name

--- src/GlobeApi.php
+++ src/GlobeApi.php
@@ -12,12 +12,12 @@ class GlobeApi
 	{
 		$this->client = $client;
 
-		$this->shortcode = config('globe.shortcode');
+		$this->short_code = config('globe.short_code');
 	}
 
 	public function send($number, $message, $passphrase, $app_id, $app_secret)
 	{
-		$url = 'outbound/' . $this->shortcode . '/requests';
+		$url = 'outbound/' . $this->short_code . '/requests/';
 		
 		$params = [
 			'form_params' => [
